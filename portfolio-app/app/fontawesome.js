56987c37f53bdbd2497fde97da89db13eae59553
MichaelOmoniyi <oomoniyimichael18@gmail.com>
2024-08-21 12:01:00 +0100

    Implementing fontawesome config file

---
+++ portfolio-app/app/fontawesome.js
@@ -0,0 +1,6 @@
+// fontawesome.js
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faArrowDown, faEnvelope, faFlask, faGlobe, faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faCss3, faGit, faGithub, faHtml5, faJs, faLinkedin, faPython, faTwitter } from '@fortawesome/free-brands-svg-icons';
+
+library.add(faPhone, faGlobe, faEnvelope, faGithub, faLinkedin, faTwitter, faGit, faPython, faHtml5, faCss3, faArrowDown);

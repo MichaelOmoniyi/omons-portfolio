8650adcca63cff496898966cfe9c315bcbc3077f
MichaelOmoniyi <oomoniyimichael18@gmail.com>
2024-08-23 14:12:11 +0100

    Modifying hamburger styles

--- portfolio-app/app/Components/Hamburger/Hamburger.jsx
+++ portfolio-app/app/Components/Hamburger/Hamburger.jsx
@@ -1,10 +1,31 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./Hamburger.module.css";
 
 const Humburger = ({ onHamburgerClicking }) => {
+  useEffect(() => {
+    const hamburger = document.getElementById("hamburger");
+    const screenHeight =
+      window.innerHeight ||
+      document.body.clientHeight ||
+      document.documentElement.clientHeight;
+    let lastScrollTop = window.scrollY || document.documentElement.scrollTop;
+
+    window.addEventListener("scroll", () => {
+      let scrollTopPosition =
+        window.scrollY || document.documentElement.scrollTop;
+      if (window.scrollY > screenHeight) {
+        hamburger.classList.add(`${styles.hamburgerScroll}`);
+      } else {
+        hamburger.classList.remove(`${styles.hamburgerScroll}`);
+      }
+
+      lastScrollTop = scrollTopPosition <= 0 ? 0 : scrollTopPosition;
+    });
+  });
+
   const handleClick = (event) => {
-    if(onHamburgerClicking) {
+    if (onHamburgerClicking) {
       onHamburgerClicking("nav-btn");
     }
   };
@@ -17,7 +38,11 @@ const Humburger = ({ onHamburgerClicking }) => {
         className={styles.navBtn}
       />
       <label htmlFor="nav-btn" className={styles.navBtnLabel}>
-        <span className={styles.hamburger} onClick={handleClick}></span>
+        <span
+          className={styles.hamburger}
+          id="hamburger"
+          onClick={handleClick}
+        ></span>
       </label>
     </div>
   );

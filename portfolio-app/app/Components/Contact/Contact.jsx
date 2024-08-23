82423c73d3d8d1a70ba34aad984affcfeb94b410
MichaelOmoniyi <oomoniyimichael18@gmail.com>
2024-08-23 15:22:43 +0100

    Adding links to icons

--- portfolio-app/app/Components/Contact/Contact.jsx
+++ portfolio-app/app/Components/Contact/Contact.jsx
@@ -5,33 +5,43 @@ import { faPhone } from "@fortawesome/free-solid-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
-import styles from './Contact.module.css'
+import styles from "./Contact.module.css";
 
 const Contact = () => {
   return (
     <div className="container" id="contact">
       <h2 className="header">Contact</h2>
       <div className={styles.contacts}>
-        <div className={styles.contact}>
-          <FontAwesomeIcon icon={faPhone} className={styles.icons} />
-          <p className={styles.detail}>08186897876, 08152889069</p>
-        </div>
-        <div className={styles.contact}>
-          <FontAwesomeIcon icon={faEnvelope} className={styles.icons} />
-          <p className={styles.detail}>oomoniyimicheal18@gmail.com</p>
-        </div>
-        <div className={styles.contact}>
-          <FontAwesomeIcon icon={faLinkedin} className={styles.icons} />
-          <p className={styles.detail}>MichaelOmoniyi</p>
-        </div>
-        <div className={styles.contact}>
-          <FontAwesomeIcon icon={faTwitter} className={styles.icons} />
-          <p className={styles.detail}>_omons</p>
-        </div>
-        <div className={styles.contact}>
-          <FontAwesomeIcon icon={faGithub} className={styles.icons} />
-          <p className={styles.detail}>MichaelOmoniyi</p>
-        </div>
+        <a href={`tel:08186897876`} target="_blank">
+          <div className={styles.contact}>
+            <FontAwesomeIcon icon={faPhone} className={styles.icons} />
+            <p className={styles.detail}>08186897876, 08152889069</p>
+          </div>
+        </a>
+        <a href={`mailto:oomoniyimicheal18@gmail.com`} target="_blank">
+          <div className={styles.contact}>
+            <FontAwesomeIcon icon={faEnvelope} className={styles.icons} />
+            <p className={styles.detail}>oomoniyimicheal18@gmail.com</p>
+          </div>
+        </a>
+        <a href="https://bit.ly/40It02c" target="_blank">
+          <div className={styles.contact}>
+            <FontAwesomeIcon icon={faLinkedin} className={styles.icons} />
+            <p className={styles.detail}>MichaelOmoniyi</p>
+          </div>
+        </a>
+        <a href="https://twitter.com/_omons" target="_blank">
+          <div className={styles.contact}>
+            <FontAwesomeIcon icon={faTwitter} className={styles.icons} />
+            <p className={styles.detail}>_omons</p>
+          </div>
+        </a>
+        <a href="https://github.com/MichaelOmoniyi" target="_blank">
+          <div className={styles.contact}>
+            <FontAwesomeIcon icon={faGithub} className={styles.icons} />
+            <p className={styles.detail}>MichaelOmoniyi</p>
+          </div>
+        </a>
       </div>
     </div>
   );

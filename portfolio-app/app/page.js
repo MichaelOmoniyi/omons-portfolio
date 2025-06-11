4732b1c6a763dc449697762a2d35183fa8018450
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2025-06-11 17:22:05 +0100

    Add new sections

--- portfolio-app/app/page.js
+++ portfolio-app/app/page.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import NavBar from './Components/Navbar/NavBar'
-import LandingPage from './Components/LandingPage/LandingPage'
-import About from './Components/About/About'
-import Skills from './Components/Skills/Skills'
-import Projects from './Components/Projects/Projects'
-import Contact from './Components/Contact/Contact'
-import SayHi from './Components/SayHi/SayHi'
-import Footer from './Components/Footer/Footer'
+import React from "react";
+import NavBar from "./Components/Navbar/NavBar";
+import LandingPage from "./Components/LandingPage/LandingPage";
+import About from "./Components/About/About";
+import Skills from "./Components/Skills/Skills";
+import Projects from "./Components/Projects/Projects";
+import Blog from "./Components/Blog/Blog"
+import GitHubStats from "./Components/GitHubStats/GitHubStats";
+import Contact from "./Components/Contact/Contact";
+import SayHi from "./Components/SayHi/SayHi";
+import Footer from "./Components/Footer/Footer";
 
 const Home = () => {
   return (
@@ -19,12 +21,13 @@ const Home = () => {
         <About />
         <Skills />
         <Projects />
+        <Blog />
+        <GitHubStats />
         <Contact />
         <SayHi />
         <Footer />
       </main>
     </div>
   );
-}
-
-export default Home
+};
+export default Home;
